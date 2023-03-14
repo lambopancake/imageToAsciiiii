97dbc2fc3ed0bbebd41698717fddabfa41a4c4fd
Lambopancake <racarrazco@gmail.com>
2023-03-14 01:14:34 -0400

    update

--- image_to_asciiiii.py
+++ image_to_asciiiii.py
@@ -3,11 +3,12 @@ import sys, os.path
 
 class picToAscii():
 
-	def __init__(self, argv):
+	def __init__(self, argv, c):
 		#to lazy to flip entire string in reverse
 		res = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'.          "
 		self.res = res[::-1]
 		self.name = argv
+		self.clar = c
 
 # usage = "python3 image_to_asciiiii.py [*.png]"
 # argv = sys.argv[1]
@@ -26,24 +27,20 @@ class picToAscii():
 		return True
 
 #opens the image and converts to black and white
-	def toAscii(self):
+	def toAscii_BlackWhite(self):
 		image = cv2.imread(f"{self.name}")
 		image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-	
-
-# cv2.imshow("hi",image)
-# cv2.waitKey(0)
 
 		x = image.shape[0]
 		y = image.shape[1]
 
-		a = 15 #ascii image res
+		 #ascii image res
 
 		fileOut = open(f'{self.name[:-4]}_ascii.txt','w') #opens file
 
 		#prints to file by pixel intensity
-		for i in range(0,x,a):
-			for j in range(0,y,a):
+		for i in range(0,x,self.clar):
+			for j in range(0,y,self.clar):
 				inte = int(image[i,j])
 				#print(f"{self.res[self.per(inte,len(self.res))]}",file = fileOut, end = " ")
 				print(f"{self.res[self.per(inte,len(self.res))]}", end = " ")
@@ -54,9 +51,10 @@ class picToAscii():
 
 	def main(self):
 		if(self.check()):
-			self.toAscii()
+			self.toAscii_BlackWhite()
 
 if __name__ == "__main__":
-	argv = sys.argv[1]
-	a = picToAscii(argv)
+	argv_file = sys.argv[1]
+	argv_clar = int(sys.argv[2])
+	a = picToAscii(argv_file, argv_clar)
 	a.main()
