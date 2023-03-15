912249d6f3cecfeaeb01411f620274556449d1e4
Lambopancake <racarrazco@gmail.com>
2023-03-14 22:04:02 -0400

    Fix video stutter

--- image_to_asciiiii.py
+++ image_to_asciiiii.py
@@ -3,7 +3,7 @@ import sys, os.path
 
 class picToAscii():
 
-	def __init__(self, argv, c):
+	def __init__(self ,c , argv = None):
 		#to lazy to flip entire string in reverse
 		res = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'.          "
 		self.res = res[::-1]
@@ -30,34 +30,41 @@ class picToAscii():
 
 
 	#opens the image and converts to black and white
-	def toAscii_BlackWhite(self):
-		image = cv2.imread(f"{self.name}")
+	def toAscii_BlackWhite(self, pic, f = 0, toFile = 0):
+		if(f == 1):
+			image = cv2.imread(f"{pic}")
+		else:	
+			image = pic
 		imageBW = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
 		x = image.shape[0]
 		y = image.shape[1]
 
 		 #ascii image res
-
-		fileOut = open(f'{self.name[:-4]}_ascii.txt','w') #opens file
+		if(f == 1):
+			fileOut = open(f'{self.name[:-4]}_ascii.txt','w') #opens file
 
 		#prints to file by pixel intensity
 		for i in range(0,x,self.clar):
 			for j in range(0,y,self.clar):
-				inte = int(imagBWe[i,j])
-				#print(f"{self.res[self.per(inte,len(self.res))]}",file = fileOut, end = " ")
-				print(f"{self.res[self.per(inte,len(self.res))]}", end = " ")
-			#print("",file = fileOut)
-			print("")
-
-		fileOut.close()
+				inte = int(imageBW[i,j])
+				if(toFile == 1):
+					print(f"{self.res[self.per(inte,len(self.res))]}",file = fileOut, end = " ")
+				else:
+					print(f"{self.res[self.per(inte,len(self.res))]}", end = " ")
+			if(toFile == 1):
+				print("",file = fileOut)
+			else:
+				print("")
+		if(f == 1):
+			fileOut.close()
 
 	def main(self):
 		if(self.check()):
-			self.toAscii_BlackWhite()
+			self.toAscii_BlackWhite(self.name, f = 1)
 
 if __name__ == "__main__":
 	argv_file = sys.argv[1]
 	argv_clar = int(sys.argv[2])
-	a = picToAscii(argv_file, argv_clar)
+	a = picToAscii(argv_clar, argv_file)
 	a.main()
--- vidToMatrix.py
+++ vidToMatrix.py
@@ -1,53 +1,30 @@
-import cv2,os
+import cv2,os,time
+from image_to_asciiiii import picToAscii
 
 
-def per(inte, resLen):
-	return int((inte/255.0)* 100 * ((resLen-1)*0.01)) 
-
-def printStuff(imageBW, x, y):
-
-	res = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'.          "
-	res = res[::-1]
-	clar = 15
-	for i in range(0,x,clar):
-		for j in range(0,y,clar):
-			#print(i ," ", j)
-			try:
-				inte = int(imageBW[i,j])
-			except:
-				print("",end = "")
-			print(f"{res[per(inte,len(res))]}", end = " ")
-		print("")
-
-vid = cv2.VideoCapture(0)
-
-width  = vid.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
-height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`
-# or
-width  = vid.get(3)  # float `width`
-height = vid.get(4)  # float `height`
-
-print('width, height:', width, height)
-
-x = int(width)
-y = int(height) 
 
 
-i = 0
-os.system('cls')
-while i < 1000:
+if __name__ == "__main__":
+	vid = cv2.VideoCapture(0)
+	i = 0
+	os.system('cls')
+	while i < 100:
+		
+		time.sleep(0.01)
+		
+		os.system("clear")
+
+		frame = vid.read()[1]
+		blackAndWhite = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		a = picToAscii(15)
+		a.toAscii_BlackWhite(frame)
 	
-	frame = vid.read()[1]
-	blackAndWhite = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	#print(blackAndWhite)
-	#cv2.imshow("sup", blackAndWhite)
-	
-	printStuff(blackAndWhite,x, y)
-	os.system('clear')
-	key = cv2.waitKey(20)
-	if key == 27: #hit esc to quit
-		break
-	i += 1
 
-vid.release()
-#cv2.destroyWindow()
+
+		key = cv2.waitKey(20)
+		if key == 27: #hit esc to quit
+			break
+		i += 1
+
+	vid.release()
+	#cv2.destroyWindow()
